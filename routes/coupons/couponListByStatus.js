95347e2d468e9307598d8fb04563bdb7ecb42db0
fanzehua <316679581@qq.com>
2022-06-17 17:24:57 +0800

    添加兑换优惠券接口，添加查看优惠券列表

---
+++ routes/coupons/couponListByStatus.js
@@ -0,0 +1,27 @@
+/**
+ * 根据状态分类获取优惠券列表接口
+ */
+const router = require("koa-router")();
+const Coupons = require("../../models/couponsSchema");
+const util = require("../../utils/util");
+const log4j = require("../../utils/log4");
+const dayjs = require("dayjs");
+
+router.post("/coupons/redeem", async (ctx) => {
+  try {
+    const { status } = ctx.request.body; // 1 全部 2 未使用 3 已使用
+    const { page, skipIndex } = util.pager(ctx.request.body);
+    const { user } = ctx.state;
+    let params = { belongUser: user._id };
+    if (status !== 1) {
+      params = { status, ...params };
+    }
+    const list = await Coupons.find(params).skip(skipIndex).limit(page.pageSize);
+    const total = await Coupons.countDocuments(params);
+    ctx.body = util.success({ total, list });
+  } catch (error) {
+    ctx.body = util.fail(error.stack);
+  }
+});
+
+module.exports = router;
